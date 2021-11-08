7a1edbf8f6752fb89fecc4580fa229e3e5958606
JavaStudent2021 <free.execution@gmail.com>
2021-11-08 11:09:07 +0200

    first commit

---
+++ src/java/Customer.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Customer {
+    /*
+     * field
+     * */
+    private String firstName;
+    private String lastName;
+    private String companyFromUsers;
+    private String mobPhoneNumber;
+    private String mail;
+    private String ipCustomer;
+    private String fromCity;
+    private boolean sex;
+    private float sizeShoes;
+    private float footLenght;
+    private Date birthday;
+
+    public Customer() {
+    }
+}
---
+++ src/java/Shoes.java
@@ -0,0 +1,18 @@
+public class Shoes {
+    /*
+    * field goods
+    * */
+    private String brend;
+    private String color;
+    private String model;
+    private String discription;
+    private String manufacturer;
+    private String article;
+    private String sexType;
+    private Object foto;
+    private float size;
+    private float cost;
+    private boolean sex;
+
+
+}
